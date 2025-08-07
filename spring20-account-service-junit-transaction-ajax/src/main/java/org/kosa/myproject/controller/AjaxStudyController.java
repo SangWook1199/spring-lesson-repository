808b1d65037732009cc065aee96aaa679095b58d
sangwook1199 <sangwook1199@gmail.com>
2025-08-07 10:55:47 +0900

    feat: ajax jquery

---
+++ spring20-account-service-junit-transaction-ajax/src/main/java/org/kosa/myproject/controller/AjaxStudyController.java
@@ -0,0 +1,37 @@
+package org.kosa.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// @RestController: @Controller + ResponseBody => 이후 공부
+@Controller
+public class AjaxStudyController {
+	
+	@GetMapping("/test-ajax1")
+	@ResponseBody // ajax 응답을 위한 설정, 페이지가 아니라 필요한 데이터로 응답(text or JSON)
+	public String testAjax1(String userId) {
+		// System.out.println(userId);
+		try {
+			// 서버 로직이 복잡하여 시간 소요가 많이 될 수 있다고 가정
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "hello ajax "+userId;
+	}
+	
+	@GetMapping("/test-ajax2")
+	@ResponseBody // ajax 응답을 위한 설정, 페이지가 아니라 필요한 데이터로 응답(text or JSON)
+	public String testAjax2(String userId) {
+		// System.out.println(userId);
+		try {
+			// 서버 로직이 복잡하여 시간 소요가 많이 될 수 있다고 가정
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "hello jQuery ajax "+userId;
+	}
+	
+}
---
+++ spring20-account-service-junit-transaction-ajax/src/main/java/org/kosa/myproject/controller/HomeController.java
@@ -0,0 +1,21 @@
+package org.kosa.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "index";
+	}
+	@GetMapping("/ajax-study1")
+	public String ajaxStudy1() {
+		return"ajax-study1";
+	}
+	
+	@GetMapping("/ajax-study2")
+	public String ajaxStudy2() {
+		return "ajax-study2-jquery";// thymeleaf template view
+	}
+}
